b640127b365d95020d917f158b44218070fa7864
Sylvain Girault <syl@djro.fr>
2025-10-25 18:34:15 +0200

    chore: vitest setup file

--- frontend/breco/vitest/setup.ts
+++ frontend/breco/vitest/setup.ts
@@ -2,16 +2,19 @@ import { createTestingPinia } from '@pinia/testing'
 import { setActivePinia } from 'pinia'
 
 // Mock localStorage
-Object.defineProperty(window, 'localStorage', {
-  value: {
-    getItem: () => null,
-    setItem: () => {},
-    removeItem: () => {},
-    clear: () => {},
-    key: () => null,
-    length: 0,
-  },
-})
+if (typeof window !== 'undefined') {
+  Object.defineProperty(window, 'localStorage', {
+    value: {
+      getItem: () => null,
+      setItem: () => {},
+      removeItem: () => {},
+      clear: () => {},
+      key: () => null,
+      length: 0,
+    },
+    writable: true,
+  })
+}
 
 // Initialise Pinia pour les tests
 setActivePinia(createTestingPinia())

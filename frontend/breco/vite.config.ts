d9f074b46e3d71682f309100a0281fee73db706d
Sylvain Girault <syl@djro.fr>
2025-10-25 18:43:48 +0200

    chore: fix vite config

--- frontend/breco/vite.config.ts
+++ frontend/breco/vite.config.ts
@@ -3,8 +3,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const apiUrl = process.env.VITE_API_URL || 'http://localhost:8081'
+
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    process.env.VITEST !== 'true' ? vueDevTools() : null,
+  ].filter(Boolean),
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -13,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8081',
+        target: apiUrl,
         changeOrigin: true,
         rewrite: (path) => path
       }

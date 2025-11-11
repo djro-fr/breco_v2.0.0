39e79d5bc930ebafb1682d3726511ad42c342367
Sylvain Girault <syl@djro.fr>
2025-11-11 10:36:53 +0100

    git ignore

--- backend/breco/config/bootstrap.php
+++ backend/breco/config/bootstrap.php
@@ -65,10 +65,9 @@ require CAKE . 'functions.php';
  * security risks. See https://github.com/josegonzalez/php-dotenv#general-security-information
  * for more information for recommended practices.
 */
-if (!env('APP_NAME') && file_exists(CONFIG . '.env')) {
+if (file_exists(CONFIG . '.env')) {
     $dotenv = new \josegonzalez\Dotenv\Loader([CONFIG . '.env']);
     $dotenv->parse()
-        ->putenv()
         ->toEnv()
         ->toServer();
 }
